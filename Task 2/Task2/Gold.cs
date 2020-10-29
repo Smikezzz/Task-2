94b648babb79546164d2e787f93858dcfdbc872c
Smikezzz <danieliaswart88@gmail.com>
2020-10-29 17:59:39 +0200

    Task 2 (2.3)
    Progression of task 2 with 2.3

--- Task 2/Task2/Gold.cs
+++ Task 2/Task2/Gold.cs
@@ -6,8 +6,18 @@ using System.Threading.Tasks;
 
 namespace TASK1
 {
-    class Gold
+    class Gold : Item
     {
+        private int gold;
+        private Random r = new Random();
 
+        public int GOLD { get => gold; set => gold = value; }
+        public Random R { get => r; set => r = value; }
+
+        public Gold(int x, int y) : base(x, y)
+        {
+            this.x = r.Next(1, 5 + 1);
+            this.y = r.Next(1, 5 + 1);
+        }
     }
 }
